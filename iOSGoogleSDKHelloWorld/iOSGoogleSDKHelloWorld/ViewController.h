26f93fd80017e0e33228ab668243928e0df48175
Anthony Marchenko <anthonymarchenko@gmail.com>
2014-01-24 00:17:29 +0300

    Init project

---
+++ iOSGoogleSDKHelloWorld/iOSGoogleSDKHelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOSGoogleSDKHelloWorld
+//
+//  Created by Anthony Marchenko on 1/24/14.
+//  Copyright (c) 2014 Anthony Marchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
